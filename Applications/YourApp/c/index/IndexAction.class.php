f2ddaa31036142f2c2a3673ac9b536cf4f5cb9c3
sjwidLberes <yurena19610407@126.com>
2021-04-29 09:52:32 +0800

    IndexAction  荷官所有发送的数据在此处理

--- Applications/YourApp/c/index/IndexAction.class.php
+++ Applications/YourApp/c/index/IndexAction.class.php
@@ -1,6 +1,6 @@
 <?php /**
  *  【梦想cms】 http://www.lmxcms.com *
- *   前台首页控制器
+ *   荷官操作控制器
  *   不需要验证连接token的  直接继承Action   不在继承HomeAction
  */
 defined('LMXCMS') or exit();
@@ -8,9 +8,15 @@ class IndexAction extends HomeAction{
     public function __construct() {
         parent::__construct();
     }
+
+    //房间初始化方法 接收荷官的ID  添加游戏开始计时器
     public function index(){
         echo "首页控制器";
     }
 
+    //荷官发送打点数
+    public function sendnum(){
+        
+    }
 }
 ?>
