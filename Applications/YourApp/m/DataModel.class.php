92fd8c2a05e5eb4ad3c82c2cedc3d6833ded4e36
sjwidLberes <yurena19610407@126.com>
2021-04-29 23:44:19 +0800

    mac

--- Applications/YourApp/m/DataModel.class.php
+++ Applications/YourApp/m/DataModel.class.php
@@ -70,5 +70,10 @@ class DataModel{
         $this->setRoomData($roomid,$roomData);
         return true;
     }
+    //todo 取得游戏状态
+    public function getGameStatus($roomid){
+        $roomData=$this->getRoomData($roomid);
+        return $roomData['status'];
+    }
 }
 ?>
